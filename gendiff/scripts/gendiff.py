b686af4584093d5e33c5f82005fd4ef6618b3d98
margisk <nickoskuzin@gmail.com>
2026-05-21 17:41:56 +0200

    add -format option

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,16 +1,20 @@
 import argparse
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser(
-        description="Compares two configuration files and shows a difference.")
-    parser.add_argument('first_file', nargs='*')
-    parser.add_argument('second_file', nargs='*')
+        description="Compares two configuration files and shows a difference."
+    )
+    parser.add_argument("first_file", nargs="*")
+    parser.add_argument("second_file", nargs="*")
+    parser.add_argument("-f", "--format", help="set format of output")
     args = parser.parse_args()
-    parser.print_help()
     return args
+
 
 def main():
     args = parse_arguments()
+
 
 if __name__ == "__main__":
     main()
